1dbefa44096411016d7c82e2d7e373192a31bded
NWALOZIE AMAMIHE CHIBUZOR <44822642+nacbotics5@users.noreply.github.com>
2021-02-02 01:06:15 +0100

    Fixed and updated get_members.py

--- get_members.py
+++ get_members.py
@@ -36,7 +36,61 @@ try:
     # get all the users and print them
     for user in client.get_participants(channel):
         name = f"{user.first_name} {user.last_name}"
-        save_members(user.username,user.id,user.access_hash,name)
+        save_members(user.username,user.id,user.access_hash,name)import csv
+from config import*
+from telethon import TelegramClient, sync
+
+
+client = TelegramClient(phone, api_id, api_hash)
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+
+
+
+
+def get_channels():
+    # get all the channels that I can access
+    channels = {d.entity.username: d.entity
+            for d in client.get_dialogs()
+            if d.is_channel}
+    for i,channel_name in enumerate(channels.keys()):
+        try:print(f"{i} ::: {channel_name}")
+        except:pass
+
+    channel_id = int(input("Please select a number :: "))
+
+    channel_name = list(channels.keys())[channel_id]
+    channel = channels[channel_name]
+    print(f"\n\nYou have selected:: {channel_name}")
+
+    print("\n\nGetting the members of this channel\n")
+
+    try:
+        with open('tbot.csv', mode='w+') as csv_file:
+            fieldnames = ['username','id','access_hash','name']
+            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            writer.writeheader()
+            
+            # get all the users and print them
+            for user in client.get_participants(channel):
+                name = f"{user.first_name} {user.last_name}"
+                writer.writerow({'username': user.username,'id':user.id,'access_hash':user.access_hash,'name':name})
+                print(user.username,user.id,user.access_hash,name)
+    except Exception as e:
+		    print(e)
+		    get_channels()
+		    
+try:
+    get_channels()
+except Exception as e:
+    print(e)
+		     
+
+
+
         print(user.username,user.id,user.access_hash,name)
 except Exception as e:
 		print(e)
